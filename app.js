37c928b311e516e6ccee879d646746dd18932bb0
JeaHoSong <jeaho2552@gmail.com>
2023-06-19 01:47:56 +0900

    추가: prettier 적용

--- app.js
+++ app.js
@@ -2,39 +2,39 @@ const container = document.getElementById('root');
 const ajax = new XMLHttpRequest();
 const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
-const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json'
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
-const getData = (url) => {
-    ajax.open('GET', url, false);
-    ajax.send();
+const getData = url => {
+  ajax.open('GET', url, false);
+  ajax.send();
 
-    return JSON.parse(ajax.response);
-}
+  return JSON.parse(ajax.response);
+};
 
 const newsFeed = getData(NEWS_URL);
 const ul = document.createElement('ul');
 
-window.addEventListener('hashchange', function() {
-    const id = location.hash.substr(1);
+window.addEventListener('hashchange', function () {
+  const id = location.hash.substr(1);
 
-    const newsContent = getData(CONTENT_URL.replace('@id', id));
-    const title = this.document.createElement('h1');
+  const newsContent = getData(CONTENT_URL.replace('@id', id));
+  const title = this.document.createElement('h1');
 
-    title.innerHTML = newsContent.title;
-    content.appendChild(title);
+  title.innerHTML = newsContent.title;
+  content.appendChild(title);
 });
 
-for(let i=0; i<10; i++){
-    const div = document.createElement('div');
+for (let i = 0; i < 10; i++) {
+  const div = document.createElement('div');
 
-    div.innerHTML = `
+  div.innerHTML = `
         <li>
             <a href='#${newsFeed[i].id}'>
                 ${newsFeed[i].title} (${newsFeed[i].comments_count})
             </a>
-        </li>`
+        </li>`;
 
-    ul.appendChild(div.firstElementChild);
+  ul.appendChild(div.firstElementChild);
 }
 
 container.appendChild(ul);

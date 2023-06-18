fc4eb2fcccaf2dbbad9e4ec5cccb404e8d46eca3
JeaHoSong <jeaho2552@gmail.com>
2023-06-19 01:22:36 +0900

    추가: 제목 출력하기

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+const container = document.getElementById('root');
 const ajax = new XMLHttpRequest();
+const content = document.createElement('div');
 const NEW_URL = 'https://api.hnpwa.com/v0/news/1.json';
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json'
 
 ajax.open('GET', NEW_URL, false);
 ajax.send();
@@ -7,15 +10,29 @@ ajax.send();
 const newsFeed = JSON.parse(ajax.response);
 const ul = document.createElement('ul');
 
+window.addEventListener('hashchange', function() {
+    const id = location.hash.substr(1);
+
+    ajax.open('GET', CONTENT_URL.replace('@id', id), false);
+    ajax.send();
+
+    const newsContent = JSON.parse(ajax.response);
+    const title = this.document.createElement('h1');
+
+    title.innerHTML = newsContent.title;
+    content.appendChild(title);
+});
+
 for(let i=0; i<10; i++){
     const li = document.createElement('li');
     const a = document.createElement('a');
 
-    a.href = '#';
+    a.href = `#${newsFeed[i].id}`;
     a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
 
     li.appendChild(a);
     ul.appendChild(li);
 }
 
-document.getElementById('root').appendChild(ul);
+container.appendChild(ul);
+container.appendChild(content);

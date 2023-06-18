b0b03482396a8cc480a2210c5d090e881dbf6449
JeaHoSong <jeaho2552@gmail.com>
2023-06-19 02:08:10 +0900

    추가: 기본 라우팅

--- app.js
+++ app.js
@@ -11,31 +11,47 @@ const getData = url => {
   return JSON.parse(ajax.response);
 };
 
-const newsFeed = getData(NEWS_URL);
-const ul = document.createElement('ul');
+const getNewsFeed = () => {
+  const newsFeed = getData(NEWS_URL);
+  const newsList = [];
 
-window.addEventListener('hashchange', function () {
-  const id = location.hash.substr(1);
-
-  const newsContent = getData(CONTENT_URL.replace('@id', id));
-  const title = this.document.createElement('h1');
-
-  title.innerHTML = newsContent.title;
-  content.appendChild(title);
-});
-
-for (let i = 0; i < 10; i++) {
-  const div = document.createElement('div');
-
-  div.innerHTML = `
+  newsList.push('<ul>');
+  for (let i = 0; i < 10; i++) {
+    newsList.push(`
         <li>
             <a href='#${newsFeed[i].id}'>
                 ${newsFeed[i].title} (${newsFeed[i].comments_count})
             </a>
-        </li>`;
+        </li>`);
+  }
+  newsList.push('</ul>');
 
-  ul.appendChild(div.firstElementChild);
-}
+  container.innerHTML = newsList.join('');
+};
 
-container.appendChild(ul);
-container.appendChild(content);
+const getNewsDetail = () => {
+  const id = location.hash.substr(1);
+  const newsContent = getData(CONTENT_URL.replace('@id', id));
+
+  container.innerHTML = `
+    <h1>${newsContent.title}</h1>
+
+    <div>
+        <a href='#'>목록으로</a>
+    </div>
+  `;
+};
+
+const router = () => {
+  const routePath = location.hash;
+
+  if (routePath === '') {
+    getNewsFeed();
+  } else {
+    getNewsDetail();
+  }
+};
+
+window.addEventListener('hashchange', router);
+
+router();

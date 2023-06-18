071a2f96bb92d5f468e1a2d1a8d62e02f3c4da05
JeaHoSong <jeaho2552@gmail.com>
2023-06-19 02:24:45 +0900

    추가: 페이징

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const ajax = new XMLHttpRequest();
 const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
+const store = {
+  currentPage: 1,
+};
 
 const getData = url => {
   ajax.open('GET', url, false);
@@ -16,28 +19,34 @@ const getNewsFeed = () => {
   const newsList = [];
 
   newsList.push('<ul>');
-  for (let i = 0; i < 10; i++) {
+  for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     newsList.push(`
         <li>
-            <a href='#${newsFeed[i].id}'>
+            <a href='#/show/${newsFeed[i].id}'>
                 ${newsFeed[i].title} (${newsFeed[i].comments_count})
             </a>
         </li>`);
   }
   newsList.push('</ul>');
+  newsList.push(`
+    <div>
+        <a href='#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}'>이전 페이지</a>
+         <a href='#/page/${store.currentPage < 3 ? store.currentPage + 1 : 3}'>다음 페이지</a>
+    </div>
+  `);
 
   container.innerHTML = newsList.join('');
 };
 
 const getNewsDetail = () => {
-  const id = location.hash.substr(1);
+  const id = location.hash.substr(7);
   const newsContent = getData(CONTENT_URL.replace('@id', id));
 
   container.innerHTML = `
     <h1>${newsContent.title}</h1>
 
     <div>
-        <a href='#'>목록으로</a>
+        <a href='#/page/${store.currentPage}'>목록으로</a>
     </div>
   `;
 };
@@ -46,6 +55,9 @@ const router = () => {
   const routePath = location.hash;
 
   if (routePath === '') {
+    getNewsFeed();
+  } else if (routePath.indexOf('#/page/') >= 0) {
+    store.currentPage = Number(routePath.substr(7));
     getNewsFeed();
   } else {
     getNewsDetail();
